d3514516f62305b127ebcd7e9a9513db6c0d6738
donfreddy <freddytamwo@gmail.com>
2026-03-13 18:54:29 +0100

    feat: implement Album and Artist classes with properties and constructors for handling album and artist data

--- lib/features/library/domain/entities/album.dart
+++ lib/features/library/domain/entities/album.dart
@@ -1,1 +1,15 @@
-class Album {}
+class Album {
+  final String id;
+  final String name;
+  final String artist;
+  final int trackCount;
+  final String? artworkPath;
+
+  const Album({
+    required this.id,
+    required this.name,
+    required this.artist,
+    required this.trackCount,
+    this.artworkPath,
+  });
+}
--- lib/features/library/domain/entities/artist.dart
+++ lib/features/library/domain/entities/artist.dart
@@ -1,1 +1,13 @@
-class Artist {}
+class Artist {
+  final String id;
+  final String name;
+  final int albumCount;
+  final int trackCount;
+
+  const Artist({
+    required this.id,
+    required this.name,
+    required this.albumCount,
+    required this.trackCount,
+  });
+}
